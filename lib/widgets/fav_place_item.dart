5e575235389813869f97293a3e43b8b32e9f51cb
Jaycee Buarao <jayceebuarao@gmail.com>
2023-10-12 22:51:56 -0700

    add onTap call function

--- lib/widgets/fav_place_item.dart
+++ lib/widgets/fav_place_item.dart
@@ -2,13 +2,21 @@ import 'package:favorite_places/models/place.dart';
 import 'package:flutter/material.dart';
 
 class FavoritePlaceItem extends StatelessWidget {
-  const FavoritePlaceItem({super.key, required this.place});
+  const FavoritePlaceItem({
+    super.key,
+    required this.place,
+    required this.onTap,
+  });
 
   final Place place;
+  final void Function(Place place) onTap;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onTap: () {
+        onTap(place);
+      },
       title: Text(place.title),
     );
   }
