86973d02b17115fe8f673f3c643a05fb4c06e4c5
Jaycee Buarao <jayceebuarao@gmail.com>
2023-10-12 20:28:05 -0700

    add StateNotifier and StateNotifierProvider

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -1,3 +1,21 @@
+import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final favoritePlacesProvider = Provider((ref) => []);
+class FavoritePlacesNotifier extends StateNotifier<List<Place>> {
+  FavoritePlacesNotifier() : super([]);
+
+  void addFavoritePlace(Place place) {
+    state = [...state, place];
+  }
+
+  void removeFavoritePlace(String placeId) {
+    state = [
+      for (final place in state)
+        if (place.id != placeId) place,
+    ];
+  }
+}
+
+final favoritePlacesProvider =
+    StateNotifierProvider<FavoritePlacesNotifier, List<Place>>(
+        (ref) => FavoritePlacesNotifier());
