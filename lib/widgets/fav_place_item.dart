c746d8cef604b0a89db376b794d6a30d899cb051
Jaycee Buarao <jayceebuarao@gmail.com>
2023-10-12 22:26:44 -0700

    add dynamic title text

--- lib/widgets/fav_place_item.dart
+++ lib/widgets/fav_place_item.dart
@@ -1,12 +1,15 @@
+import 'package:favorite_places/models/place.dart';
 import 'package:flutter/material.dart';
 
 class FavoritePlaceItem extends StatelessWidget {
-  const FavoritePlaceItem({super.key});
+  const FavoritePlaceItem({super.key, required this.place});
+
+  final Place place;
 
   @override
   Widget build(BuildContext context) {
-    return const ListTile(
-      title: Text('dummy text'),
+    return ListTile(
+      title: Text(place.title),
     );
   }
 }
