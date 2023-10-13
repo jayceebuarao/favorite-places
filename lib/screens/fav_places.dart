7278e9aef092fd57409ca365461c58e605d98abe
Jaycee Buarao <jayceebuarao@gmail.com>
2023-10-12 22:51:28 -0700

    add openPlaceDetail function

--- lib/screens/fav_places.dart
+++ lib/screens/fav_places.dart
@@ -1,4 +1,6 @@
+import 'package:favorite_places/models/place.dart';
 import 'package:favorite_places/providers/favorites_provider.dart';
+import 'package:favorite_places/screens/fav_place_detail.dart';
 import 'package:favorite_places/screens/new_place.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -22,6 +24,11 @@ class _FavoritePlacesScreenState extends ConsumerState<FavoritePlacesScreen> {
     );
   }
 
+  void openPlaceDetail(Place place) {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => FavoritePlaceDetail(place: place)));
+  }
+
   @override
   Widget build(BuildContext context) {
     final favoritePlaces = ref.watch(favoritePlacesProvider);
@@ -36,6 +43,7 @@ class _FavoritePlacesScreenState extends ConsumerState<FavoritePlacesScreen> {
       body: ListView.builder(
           itemBuilder: (context, index) => FavoritePlaceItem(
                 place: favoritePlaces[index],
+                onTap: openPlaceDetail,
               ),
           itemCount: favoritePlaces.length),
     );
