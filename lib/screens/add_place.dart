2f351a8c0ca08123073226d626ac469ed636a433
Jaycee Buarao <jayceebuarao@gmail.com>
2023-10-13 21:23:17 -0700

    add locationInput widget

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:favorite_places/models/place.dart';
 import 'package:favorite_places/providers/favorites_provider.dart';
 import 'package:favorite_places/widgets/image_input.dart';
+import 'package:favorite_places/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -75,6 +76,8 @@ class _NewPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                   _selectedImage = image;
                 },
               ),
+              const SizedBox(height: 10),
+              LocationInput(),
               const SizedBox(height: 16),
               ElevatedButton.icon(
                 onPressed: _savePlace,
