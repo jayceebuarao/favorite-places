2a2e0920a18f68929f8ce883eba84f4be04578a6
Jaycee Buarao <jayceebuarao@gmail.com>
2023-10-25 20:51:22 -0700

    refactor: change addFavoritePlace arguments

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -1,11 +1,23 @@
+import 'dart:io';
+
 import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 
 class FavoritePlacesNotifier extends StateNotifier<List<Place>> {
   FavoritePlacesNotifier() : super([]);
 
-  void addFavoritePlace(Place place) {
-    state = [...state, place];
+  void addFavoritePlace(
+      String title, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+
+    final newPlace =
+        Place(title: title, image: copiedImage, location: location);
+    state = [newPlace, ...state];
   }
 
   void removeFavoritePlace(String placeId) {
